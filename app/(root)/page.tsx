8f2b4ae33bf6e523e25a3db45cc1e643538571ef
rizzra <rizzra26@gmail.com>
2024-02-09 08:43:47 +0700

    CWA-002 : SETUP - Folders Setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant={"purple"} size={"lg"}>Click Me</Button>
+      <Button variant={"purple"} size={"lg"}>
+        Click Me
+      </Button>
     </div>
   );
 }
